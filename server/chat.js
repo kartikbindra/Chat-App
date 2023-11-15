86a78eb26c967b78bfcae9c6e2e3f5bc0b432188
Kartik <kartikbindra_23cs211@dtu.ac.in>
2023-11-15 12:12:03 +0530

    cors oigin change

--- server/chat.js
+++ server/chat.js
@@ -5,7 +5,7 @@ const httpServer = createServer()
 const PORT = process.env.PORT || 5500
 const io = new Server(httpServer, {
     cors: {
-        origin: ["http://localhost:5500", "http://127.0.0.1:5500"]
+        origin: '*'
     }
 })
 
